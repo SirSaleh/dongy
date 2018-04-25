64c791ccc07b3fc41e9eae2b547de8e2cde139e7
SirSaleh <animatorsaleh@gmail.com>
2018-04-25 12:37:26 +0430

    login_required decorator added for main index

--- dongy/views.py
+++ dongy/views.py
@@ -4,8 +4,10 @@ from django.template import loader
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # views
+@login_required
 def index(request):
     index_template = loader.get_template("index.html")
     return HttpResponse(index_template.render({},request))
