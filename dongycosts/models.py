27ca903d22f6d03bafa873d2022dc6a2d2be7d6c
SirSaleh <animatorsaleh@gmail.com>
2018-05-13 20:43:27 +0430

    on_delete added for foreign keys

--- dongycosts/models.py
+++ dongycosts/models.py
@@ -5,14 +5,14 @@ from django.db import models
 
 # Create your models here.
 class friends (models.Model):
-    Username = models.ForeignKey(User)
+    Username = models.ForeignKey(User,on_delete=models.CASCADE)
     FriendName = models.CharField(max_length=20)
     class Meta:
         unique_together = ('Username', 'FriendName',)
 
 class costs (models.Model):
     CostId = models.AutoField(primary_key=True)
-    UserName = models.ForeignKey(User)
+    UserName = models.ForeignKey(User,on_delete=models.CASCADE)
     PayerName = models.CharField(max_length=20)
     FriendNames = models.CharField(max_length=20)
     FriendShare = models.CharField(max_length=1000,null=True,blank=True)
